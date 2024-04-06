9581d49a119b749cb8ce406e70d26cd016a6f367
Havondor <Havondor@gmail.com>
2024-04-05 23:50:31 -0700

    AoC 23 Day one both Parts

---
+++ src/AdventOfCode23/DayOne/DayOne.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode23.DayOne;
+
+public static class DayOne
+{
+    private static readonly string _fileName = @"Input\Day1Input.txt";
+
+    public static int PartOne()
+    {
+        return ReduceListToDigits([.. ReadInputFile().Values]).Sum();
+    }
+
+    public static int PartTwo()
+    {
+        Dictionary<int, string> calibrationValues = ReadInputFile();
+
+        SwapAlphaToDigit<DigitNameCombos>(calibrationValues);
+        SwapAlphaToDigit<DigitName>(calibrationValues);
+
+        return ReduceListToDigits([.. calibrationValues.Values]).Sum();
+    }
+
+    private static List<int> ReduceListToDigits(List<string> OGList)
+    {
+        string pattern = @"([a-zA-Z])";
+        string substitution = @"";
+
+        Regex regex = new(pattern);
+
+        return OGList.Select(x => regex.Replace(x, substitution))
+                     .Select(s => int.Parse(string.Concat(s.First(), s.Last()))).ToList();
+    }
+
+    private static void SwapAlphaToDigit<T>(Dictionary<int, string> keyValuePairs) where T : Enum
+    {
+        foreach (var digit in Enum.GetValues(typeof(T)))
+        {
+            foreach (var cv in keyValuePairs.Where(x => x.Value.Contains(digit.ToString()!)))
+            {
+                keyValuePairs[cv.Key] = cv.Value.Replace(digit.ToString()!, ((int)digit).ToString());
+            }
+        }
+    }
+
+    private static Dictionary<int, string> ReadInputFile()
+    {
+        string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        string combinedFileName = Path.Combine(currentDirectory, _fileName);
+        string filePath = Path.GetFullPath(combinedFileName);
+
+        return File.ReadAllLines(filePath)
+                   .Select((v, k) => new { Key = k, Value = v })
+                   .ToDictionary(o => o.Key, o => o.Value);
+    }
+}
---
+++ src/AdventOfCode23/Program.cs
@@ -0,0 +1,4 @@
+﻿using AdventOfCode23.DayOne;
+
+Console.WriteLine($"DayOne Part 1: {DayOne.PartOne()}");
+Console.WriteLine($"DayOne Part 2: {DayOne.PartTwo()}");
