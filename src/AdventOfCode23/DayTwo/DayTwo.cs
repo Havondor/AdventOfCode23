c6ea057cd8c1c50a4ba791b4c6a06259f3ff2301
Havondor <Havondor@gmail.com>
2024-04-13 21:48:16 -0700

    Day Two done

---
+++ src/AdventOfCode23/DayTwo/DayTwo.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace AdventOfCode23.DayTwo;
+
+public static class DayTwo
+{
+    public static int PartOne(int maxBlue, int maxGreen, int maxRed)
+    {
+        var games = FileProcessor.GetGames();
+
+        EvaluateGame evaluateGame = new(games);
+
+        return evaluateGame.GetValidGameIDs(maxBlue, maxGreen, maxRed).Sum();
+    }
+
+    public static int PartTwo()
+    {
+        var games = FileProcessor.GetGames();
+
+        EvaluateGame evaluateGame = new(games);
+
+        return evaluateGame.GetGamePowers().Sum();
+
+    }
+}
--- src/AdventOfCode23/DayTwo/Game.cs
+++ src/AdventOfCode23/DayTwo/Game.cs
@@ -5,6 +5,12 @@ public record Game
 {
     public int GameID {  get; set; }
     public List<Turn> Turns { get; set; } = default!;
+
+    public int MaxBlue => Turns.Max(x => x.Blue);
+    public int MaxGreen => Turns.Max(x =>x.Green);
+    public int MaxRed => Turns.Max(x => x.Red);
+
+    public int Power => MaxBlue * MaxGreen * MaxRed;
 }
 
 public record Turn
